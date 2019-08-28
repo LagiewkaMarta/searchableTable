3e210fb2c93b19a90611413edc632f0be21b0615
Marta <lagiewka.marta.dev@gmail.com>
2019-08-29 01:27:46 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const createSearchInput = () => {
+  const inputContainer = document.querySelector("#portexe-search-root");
+  const form = document.createElement("form");
+  form.innerHTML = `
+    <input id="portexe-input" type="text"> </input>
+    <button id="portexe-btn" type="submit"> Search </button>
+    `;
+  inputContainer.appendChild(form);
+  return form;
+};
+
+const listenOnInput = e => {
+  e.preventDefault();
+  return e.target.value;
+};
+
+const getTableData = () => {
+  const table = document.getElementById("portexe-data-table");
+  const tbody = [...table.children[1].children];
+  console.log(tbody);
+  const trows = tbody.map(el => [...el.children].map(td => td.innerText));
+
+  return trows;
+};
+const refreshTable = tableData => {
+  const tablebody = document.getElementById("portexe-data-table").children[1];
+  tablebody.innerHTML = ``;
+  tableData.forEach(_trow => {
+    const row = document.createElement("tr");
+    _trow.forEach(_td => {
+      const td = document.createElement("td");
+      td.innerText = _td;
+      row.appendChild(td);
+    });
+    tablebody.appendChild(row);
+  });
+};
+const search = (arr, searchTerm) => {
+  if (!searchTerm) {
+    return arr;
+  }
+  return arr.filter(row =>
+    row.find(el => el.toLowerCase().includes(searchTerm.toLowerCase()))
+  );
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  createSearchInput();
+  const input = document.querySelector("#portexe-input");
+  const tableData = getTableData();
+  input.addEventListener("keyup", e => {
+    const val = listenOnInput(e);
+    const currTableData = search(tableData, val);
+    refreshTable(currTableData);
+  });
+});
